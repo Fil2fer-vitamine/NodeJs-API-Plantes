0a8912ce156d8f6abbb692ad6277763e1be1abd3
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2022-11-13 15:41:13 +0100

    liaison PlanteServices.ts avec PlanteControllers.ts

--- src/PlanteServices/PlanteServices.ts
+++ src/PlanteServices/PlanteServices.ts
@@ -1,8 +1,10 @@
-import { Request, Response } from "express";
-
 class PlanteService {
-  getAllPlantes(req: Request, res: Response) {
-    return [];
+  async getAllPlantes(): Promise<any[]> {
+    console.log("PlanteServices - GET / - en fonction sur localhost:8080");
+    return ["Données", "Bouchon", "Pour test"];
   }
 }
 export default PlanteService;
+
+// La patate chaude, on ne la fait pas passer dans le Service --> effacement de la (req: Request, res: Response) --> Tout s'arrête au controller.
+// Controller va gérer la requête et va transmettre au service. C'est le Controller qui va générer la réponse : Le service n'a pas besoin des objets req: Request, res: Response.
--- src/controllers/PlanteControllers.ts
+++ src/controllers/PlanteControllers.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from "express";
+import PlanteService from "../PlanteServices/PlanteServices";
 
 // Création d'une classe
 class PlanteController {
   constructor() {} // Proriétés d'un constructor - Il n'y aura pas de paramètre à l'intérieur.
   // Méthodes à définir - Pour getAllPlantes, il nous faut un package de méthodes ... et non de propriétés ... C'est une autre utilisation des classes.
-  getAllPlantes(req: Request, res: Response) {
+  private planteService = new PlanteService();
+  async getAllPlantes(req: Request, res: Response) {
     console.log("PlanteController - GET / - en fonction sur localhost:8080");
-    res.send({ status: "OK", date: new Date(), data: [] });
+    const plantes = await this.planteService.getAllPlantes();
+    res.send({ status: "OK", date: new Date(), data: plantes });
   }
 }
 
 export default PlanteController;
+
+// async représente une méthode qui sera effectuée dès que la requête parviendra ... ce n'est pas dans l'instant ...
+// await maintient cette idée d'asychronisme ... on attend cette action ...
