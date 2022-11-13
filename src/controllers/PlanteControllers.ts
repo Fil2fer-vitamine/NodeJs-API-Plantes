2785d4481138345cb89decf16f3ae20e8c53f974
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2022-11-13 12:49:51 +0100

    mise en place de la liaison PlanteControllers.ts à PlanteRoutes.ts par première méthode .getAllPlantes

--- src/controllers/PlanteControllers.ts
+++ src/controllers/PlanteControllers.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 
 // Création d'une classe
 class PlanteController {
+  constructor() {} // Proriétés d'un constructor - Il n'y aura pas de paramètre à l'intérieur.
+  // Méthodes à définir - Pour getAllPlantes, il nous faut un package de méthodes ... et non de propriétés ... C'est une autre utilisation des classes.
   getAllPlantes(req: Request, res: Response) {}
 }
 
--- src/routes/PlanteRoutes.ts
+++ src/routes/PlanteRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import PlanteController from '../controllers/PlanteControllers';
 
 // Utilisation d'un router
 const planteRouter = Router();
 
+// Nouvelle instance de la class PlanteController
+const planteController = new PlanteController();
+
 planteRouter.get('/', (req, res) => {
-    
+    planteController.getAllPlantes(req, res);
 })
 
 // Branchement du Router dans notre index.ts : on l'exporte pour que l'index.ts puisse l'utiliser.
