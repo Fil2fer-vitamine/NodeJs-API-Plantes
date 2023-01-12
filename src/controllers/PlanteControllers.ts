5d7e1b0a06e89b5b7c0922677aa4518592bf9265
Fil <106022045+Fil2fer-vitamine@users.noreply.github.com>
2023-01-12 14:40:29 +0100

    CRUD Mise en - Service Controllers

--- src/controllers/PlanteControllers.ts
+++ src/controllers/PlanteControllers.ts
@@ -6,11 +6,37 @@ class PlanteController {
   constructor() {} // Proriétés d'un constructor - Il n'y aura pas de paramètre à l'intérieur.
   // Méthodes à définir - Pour getAllPlantes, il nous faut un package de méthodes ... et non de propriétés ... C'est une autre utilisation des classes.
   private planteService = new PlanteService();
+  
   async getAllPlantes(req: Request, res: Response) {
     console.log("PlanteController - GET / - en fonction sur localhost:8080");
     const plantes = await this.planteService.getAllPlantes();
     res.send({ status: "OK", date: new Date(), data: plantes });
   }
+  
+  async getPlanteById(req: Request, res: Response) {
+    console.log("PlanteController - GET / - en fonction sur localhost:8080");
+    const plantes = await this.planteService.getPlanteById();
+    res.send({ status: "OK", date: new Date(), data: plantes });
+  }
+  
+  async postPlante(req: Request, res: Response) {
+    console.log("PlanteController - POST / - en fonction sur localhost:8080");
+    const plantes = await this.planteService.postPlante();
+    res.send({ status: "OK", date: new Date(), data: plantes });
+  }
+  
+  async putOnePlanteById(req: Request, res: Response) {
+    console.log("PlanteController - PUT / - en fonction sur localhost:8080");
+    const plantes = await this.planteService.putOnePlanteById();
+    res.send({ status: "OK", date: new Date(), data: plantes });
+  }
+  
+  async deleteOnePlanteById(req: Request, res: Response) {
+    console.log("PlanteController - DELETE / - en fonction sur localhost:8080");
+    const plantes = await this.planteService.deleteOnePlanteById();
+    res.send({ status: "OK", date: new Date(), data: plantes });
+  }
+  
 }
 
 export default PlanteController;
