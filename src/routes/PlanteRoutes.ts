f2c79c4911c4ae20bf918373b90c9a195f42db11
Fil <106022045+Fil2fer-vitamine@users.noreply.github.com>
2023-01-12 14:33:47 +0100

    Mise en place du CRUD - Service Route

--- src/routes/PlanteRoutes.ts
+++ src/routes/PlanteRoutes.ts
@@ -12,6 +12,26 @@ planteRouter.get("/", (req, res) => {
   planteController.getAllPlantes(req, res);
 });
 
+planteRouter.get("/", (req, res) => {
+  console.log("PlanteRouter - GET / - en fonction sur localhost:8080");
+  planteController.getOnePlante(req, res);
+});
+
+planteRouter.post("/", (req, res) => {
+  console.log("PlanteRouter - POST / - en fonction sur localhost:8080");
+  planteController.postOnePlante(req, res);
+});
+
+planteRouter.put("/", (req, res) => {
+  console.log("PlanteRouter - PUT / - en fonction sur localhost:8080");
+  planteController.putOnePlanteById(req, res);
+});
+
+planteRouter.delete("/", (req, res) => {
+  console.log("PlanteRouter - DELETE / - en fonction sur localhost:8080");
+  planteController.deleteOnePlanteById(req, res);
+});
+
 // 'req' et 'res' sont deux gros objets où il y a plein de choses à l'intérieur ... Quand une requête 'req' arrive, 'express' l'associe à un objet 'res' (réponse) qui sera utile pour renvoyer des informations ou pour renvoyer des erreurs ...
 
 // Branchement du Router dans notre index.ts : on l'exporte pour que l'index.ts puisse l'utiliser.
